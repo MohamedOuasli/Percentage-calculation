9e2c70cc77f051bced572153c9f720998d55f26c
Pancakeloki <dodoouasli@gmail.com>
2026-02-17 19:47:25 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+# Raj scored 40, 70, 50 and 60 out of 100 in maths, science, Hindi and English. Find the percentage he got?
+percent1=int(input("Enter the number:"))
+percent2=int(input("Enter the number:"))
+percent3=int(input("Enter the number:"))
+percent4=int(input("Enter the number:"))
+totalpercent=percent1+percent2+percent3+percent4
+print(totalpercent/4)
